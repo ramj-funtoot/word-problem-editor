3c09bd052874753431798163b9c54c4ef59743b9
amitdawar <amit.dawar@funtoot.com>
2017-08-29 17:43:44 +0530

    mcq mmcs set to empty array when required

--- client/app/question/question.dialog/question.dialog.controller.js
+++ client/app/question/question.dialog/question.dialog.controller.js
@@ -8,6 +8,11 @@ angular.module('wpappApp')
     var langId = $scope.langId;
     $scope.users = users;
 
+    _.each($scope.item.options, function (o) {
+      if (o.mmc === null && o.answer == false)
+        o.mmc = [];
+    });
+
     $scope.meta = {
       grades: [1, 2, 3, 4, 5],
       btlos: ['Remember', 'Understand', 'Apply', 'Analyze', 'Evaluate', 'Create'],
@@ -19,13 +24,13 @@ angular.module('wpappApp')
       sub_levels: [1, 2, 3, 4, 5, 6],
       mcqType: [1, 2, 3, 4, 5, 6, 7, 8],
       locales: [{
-        id: 'en',
-        name: 'English'
-      },
-      {
-        id: 'mr',
-        name: 'Marathi'
-      }
+          id: 'en',
+          name: 'English'
+        },
+        {
+          id: 'mr',
+          name: 'Marathi'
+        }
       ],
       validate: function () {
         return true;
@@ -151,7 +156,7 @@ angular.module('wpappApp')
         .multiple(true);
       $mdDialog.show(confirm).then(function () {
         //Remove particular option
-        console.log("following was option removed :", $scope.item.options[index])
+        console.log("following option was removed :", $scope.item.options[index])
         $scope.item.options.splice(index, 1);
         //Update remaining options
         $scope.item.options.forEach(function (o) {
@@ -179,7 +184,7 @@ angular.module('wpappApp')
           item.i18n["en"]["OPT_" + r] = str;
           delete item.i18n["en"]["OPT_" + i]
         }
-      }, function () { });
+      }, function () {});
     }
 
 
